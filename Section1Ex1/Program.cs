2d599a73e313ff364289e5caaaaa2633af1e2abc
DaffodilGirl <gillhd22@gmail.com>
2020-05-29 15:16:46 +0100

    Add project files.

---
+++ Section1Ex1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Section1Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw1 = new StopWatch();
+            bool doRun = true;
+
+            while (doRun)
+            {
+                try
+                {
+                    var action = (ListOptions()).ToLower();
+                    if (action == "start") sw1.Start();
+                    else if (action == "stop") sw1.Stop();
+                    else if (action == "duration") sw1.ReportDuration();
+                    else if (action == "quit") doRun = false;
+                    else Console.WriteLine(">> Invalid Selection - try again");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+
+        static string ListOptions()
+        {
+            int min = int.MaxValue;
+            int max = 0;
+            int idx = 0;
+
+            string[] options = new string[] { "Start", "Stop", "Show Elapsed Time", "Quit" };
+            Console.WriteLine("\n>> Please select stopwatch option:");
+            foreach (var opt in options)
+            {
+                Console.WriteLine("{0,3}. {1}", ++idx, opt);
+                max = Math.Max(max, idx);
+                min = Math.Min(min, idx);
+            }
+
+            int sel = InputInteger("Input option number", min, max);
+            return (sel == 3) ? "Duration" : options[sel-1];
+        }
+
+
+        public static int InputInteger(string text, int? min, int? max)
+        {
+            bool ok = false;
+            int num = 0;
+            string range;
+
+            // Number range display
+            if (min.HasValue && max.HasValue) { range = " (" + min + "-" + max + ")"; }
+            else if (max.HasValue) { range = " (<=" + max + ")"; }
+            else if (min.HasValue) { range = " (>=" + min + ")"; }
+            else range = "";
+
+            if (!min.HasValue) { min = int.MinValue; }
+            if (!max.HasValue) { max = int.MaxValue; }
+
+            if (string.IsNullOrEmpty(text)) { text = "Input an integer"; }
+            while (!ok)
+            {
+                Console.Write("{0}{1}: ", text, range);
+                string numstr = Console.ReadLine();
+                ok = int.TryParse(numstr, out num);
+                ok = (ok && num >= min && num <= max);
+                if (!ok) Console.WriteLine("Invalid input - please try again");
+            }
+            return num;
+        }
+    }
+
+}
---
+++ Section1Ex1/StopWatch.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Section1Ex1
+{
+    public class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private bool _isActive = false;
+        private bool _hasBegun = false;
+
+        public StopWatch () { }
+
+        public TimeSpan Duration
+        {
+            get
+            {
+                var mseconds = (this._endTime - this._startTime);
+                return mseconds;
+            }
+
+        }
+
+        public void Start()
+        {
+            if (!_isActive)
+            {
+                this._startTime = DateTime.Now;
+                Console.WriteLine("--> Start Time: {0}", this._startTime.ToString("HH:mm:ss.fff"));
+                this._isActive = true;
+                this._hasBegun = true;
+            }
+            else throw new InvalidOperationException(">> Warning: Please STOP the stopwatch before restarting");
+        }
+
+
+        public void Stop()
+        {
+            if (_hasBegun && _isActive)
+            {
+                this._endTime = DateTime.Now;
+                Console.WriteLine("--> End Time: {0}", this._endTime.ToString("HH:mm:ss.fff")); 
+                this._isActive = false;
+            }
+            else throw new InvalidOperationException(">> Advice: Please START the stopwatch");
+        }
+
+
+        public void ReportDuration()
+        {
+            if (_hasBegun && !_isActive)
+            {
+                Console.WriteLine("--> Elapsed Time: {0:c}", this.Duration);
+            }
+            else throw new Exception(">> StopWatch Advice: there is no elapsed time to report");
+        }
+        
+
+    }
+
+}
